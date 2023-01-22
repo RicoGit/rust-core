91cda9d2dad30a052dc4f580cabf46f3610db660
K.Solovev <constantine.solovev@gmail.com>
2023-01-22 22:39:49 +0700

    Add test cases

--- fmt_import/src/import_manager.rs
+++ fmt_import/src/import_manager.rs
@@ -1,28 +1,14 @@
 use crate::import_manager::Import::{MultiLine, SingleLine};
 
-impl ImportProcessor {
-    pub fn new() -> Self {
-        ImportProcessor {}
-    }
-}
-
-#[derive(Clone, Default, Debug)]
-struct ImportBlock {
-    start_idx: usize,
-    end_index: usize,
-    imports: Vec<Import>,
-}
-
-#[derive(Clone, Debug)]
-enum Import {
-    SingleLine { idx: usize },
-    MultiLine { start_idx: usize, end_idx: usize },
-}
-
 /// Process imports: groups, sort, etc.
 pub struct ImportProcessor {}
 
 impl ImportProcessor {
+
+    pub fn new() -> Self {
+       ImportProcessor { }
+    }
+
     pub fn process<'c>(&self, file_content: &'c str) -> Option<&'c str> {
         // todo impl
         Some(file_content)
@@ -39,6 +25,7 @@ impl ImportProcessor {
         let mut import_block = ImportBlock::default();
 
         for (idx, line) in content.lines().enumerate() {
+            
             if line.trim().starts_with("use ") {
                 if !inside_import_block {
                     // we reach import block
@@ -48,7 +35,7 @@ impl ImportProcessor {
 
                 if line.trim().ends_with(";") {
                     // single line import
-                    import_block.imports.push(SingleLine { idx })
+                    import_block.imports.push(line.into())
                 } else {
                     start_idx_of_multiline_import.replace(idx);
                 }
@@ -59,10 +46,10 @@ impl ImportProcessor {
             // end of multiline import
             if line.trim().ends_with(";") && start_idx_of_multiline_import.is_some() {
                 let start_idx = start_idx_of_multiline_import.take().unwrap();
-                import_block.imports.push(MultiLine {
-                    start_idx,
-                    end_idx: idx,
-                });
+
+                let lines = content.lines().skip(start_idx).take(idx-start_idx).collect::<Vec<_>>();
+                let flatten = self.flatten_import(&lines);
+                import_block.imports.extend(flatten.into_iter());
                 continue;
             }
 
@@ -81,6 +68,42 @@ impl ImportProcessor {
 
         result
     }
+
+    /// Converts multi-line imports into many single-line imports
+    /// 
+    /// ```rust
+    /// // before
+    /// use std::path::{
+    ///   Path,
+    ///   PathBuf
+    /// };
+    /// // after
+    ///  use std::path::Path;
+    ///  use std::path::PathBuf;
+    /// ```
+    fn flatten_import(&self, import: &[&str]) -> Vec<String> {
+        let mut result = Vec::with_capacity(import.len()-1);
+        let prefix = import.first().unwrap().replace("{", "");
+        for line in import[1..].iter() { // without first and last
+            let clean_line = line.replace(",", "");
+            result.push(format!("{prefix}{};", clean_line.trim()))
+        }
+
+        result
+    }
+}
+
+#[derive(Clone, Default, Debug)]
+struct ImportBlock {
+    start_idx: usize,
+    end_index: usize,
+    imports: Vec<String>,
+}
+
+#[derive(Clone, Debug)]
+enum Import {
+    SingleLine { idx: usize },
+    MultiLine { start_idx: usize, end_idx: usize },
 }
 
 #[cfg(test)]
